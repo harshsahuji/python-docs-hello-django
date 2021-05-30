6a9d32a6c91a92537847bd4ad0aec32b1eb46c4b
Harsh Sahu <59276478+harshsahuji@users.noreply.github.com>
2021-05-30 16:31:44 +0530

    Update settings.py

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '12345'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ['harshsahu.me']] if 'harshsahu.me' in os.environ else []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
