f38ecce94e16484c5c1f17426f21dd43991719a1
Harsh Sahu <59276478+harshsahuji@users.noreply.github.com>
2021-05-30 16:25:12 +0530

    Update settings.py

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '12345'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ['harshsahu.me']] if 'harshsahu.me' in os.environ else []
 
